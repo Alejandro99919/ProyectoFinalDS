cb35416254e844ca7056ca8c4f46bc5693a4b11d
Kevin Escobar <kevinandresescobarpotes@gmail.com>
2024-04-10 15:00:36 -0500

    Mejoras para el Login |Check Recuérdame, Olvidaste Contraseña, Regístrate|
    -Se implementó un check para recordar la contraseña.
    
    -Se implementó un enlace  que debe dirigir a una sección para recuperar la contraseña.
    
    -Se implementó un enlace que debe redirigir a una sección para crear una cuenta.
    
    -Se mejoró el ícono de togglePassword |Mostrar/Ocultar Contraseña| (Ahora al presionarlo, se cambia el ícono por uno que hace alusión a que la contraseña está visible, y al volverlo a presionar, se cambia a uno que hace alusión a que la contraseña ya no es visible.
    
    -Ajuste del botón "Entrar", ahora tiene un mayor espaciado, mejorando el diseño general del Login.

--- ProyectoFinal/static/js/togglePassword.js
+++ ProyectoFinal/static/js/togglePassword.js
@@ -1,11 +1,14 @@
-const password = document.getElementById('password');
-const toggle = document.getElementById('iconoTogglePassword');
+document.addEventListener("DOMContentLoaded", function () {
+  const password = document.getElementById("password");
+  const toggle = document.getElementById("iconoTogglePassword");
 
-
-toggle.addEventListener('click', () =>{
-    if(password.type === 'password'){
-        password.type = 'text';
-    }else{
-        password.type = 'password';
+  toggle.addEventListener("click", () => {
+    if (password.type === "password") {
+      password.type = "text";
+      toggle.src = '{%static "imagenes/togglePasswordIcono.png" %}';
+    } else {
+      password.type = "password";
+      toggle.src = '{%static "imagenes/togglePasswordIconoOculta.png" %}';
     }
-})
+  });
+});
