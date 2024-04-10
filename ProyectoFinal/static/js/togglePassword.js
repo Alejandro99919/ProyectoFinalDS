ecd00d87f3245a436a9828379fd63650dfe3f3a6
Kevin Escobar <kevinandresescobarpotes@gmail.com>
2024-04-10 15:35:22 -0500

    Mejoras para el Login |Solución Error togglePassword|
    -Se solucionó un error que causaba que al presionar el botón de togglePassword (Mostrar/Ocultar Contraseña), en lugar de hacerlo, no se mostraba la imagen.

--- ProyectoFinal/static/js/togglePassword.js
+++ ProyectoFinal/static/js/togglePassword.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
   const password = document.getElementById("password");
   const toggle = document.getElementById("iconoTogglePassword");
 
   toggle.addEventListener("click", () => {
     if (password.type === "password") {
       password.type = "text";
-      toggle.src = '{%static "imagenes/togglePasswordIcono.png" %}';
-    } else {
+      toggle.src=togglePasswordIcono;
+    }else {
       password.type = "password";
-      toggle.src = '{%static "imagenes/togglePasswordIconoOculta.png" %}';
+      toggle.src =togglePasswordIconoOculta;
     }
   });
 });
