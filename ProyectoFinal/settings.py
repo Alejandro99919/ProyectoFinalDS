45363ac959365ed96af99980bd889bc3ddd84145
Alejandro <148273328+Alejandro99919@users.noreply.github.com>
2024-04-15 18:49:57 -0500

    Implementacion parte 1 base de datos MYSQL modelo experimental
    esta es la prueba de la base de datos de MYSQL el modelo experimental, posibles errores para reorganizar en un futuro. UltFechModificacion: 15/04/2024

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from .db import MYSQL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +74,7 @@ WSGI_APPLICATION = 'ProyectoFinal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = MYSQL
 
 
 # Password validation
