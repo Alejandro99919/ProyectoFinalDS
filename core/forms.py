f8297d8bbfa340b35551596f5887fe9b5d4c9a36
Kevin Escobar <kevinandresescobarpotes@gmail.com>
2024-04-23 15:25:12 -0500

    Personalización de login, formulario para crear cuenta, inicio y demás.
    -Se aplicó estilos a la barra de navegación, Login, Formulario, AcercaDe, etc.
    
    -Se realizaron modificaciones al código DJANGO para poder realizar una mayor personalización.

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(label='Usuario', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(label='Primer Nombre', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label='Segundo Nombre',widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Correo Electrónico',widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Contraseña',widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirmar Contraseña',widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
-	class Meta:
-		model = User
-		fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
