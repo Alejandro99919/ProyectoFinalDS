c8e17ab8ee768fe9a231ecf2dd581d76a194abdb
Kevin Escobar <kevinandresescobarpotes@gmail.com>
2024-05-01 18:14:09 -0500

    Cambios finales al código fuente.
    Se modificaron los true y false para que se mostraran como sí o no, se mejoró el Home.html, se configuró para poder conectar en remoto a MYSQL.
    
    ULTIMO COMMIT ANTES DE ENTREGA #1, QUE DIOS NOS BENDIGA Y QUE LASSO LLEGUE DE BUEN HUMOR MAÑANA Y NO NOS FUNE.

--- ProyectoFinal/db.py
+++ ProyectoFinal/db.py
@@ -1,9 +1,10 @@
 MYSQL = {
-	'default':{
-	'ENGINE': 'django.db.backends.mysql',
-	'NAME': 'onuspura-django',
-	'USER': 'root',
-	'PASSWORD': '',
-	'PORT': '3306'
-	}
+	'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onuspura-django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '192.168.18.93',  # La IP donde está alojada tu base de datos
+        'PORT': '3306',
+    }
 }
